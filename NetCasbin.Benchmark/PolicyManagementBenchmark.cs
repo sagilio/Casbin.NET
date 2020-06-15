d11a49a816bf86991fed8fb388b5aebdc9937357
Sagilio <Sagilio@outlook.com>
2020-06-15 15:09:23 +0800

    Add benchmark project

---
+++ NetCasbin.Benchmark/PolicyManagementBenchmark.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCasbin.Benchmark
+{
+    [SimpleJob(RunStrategy.Throughput, targetCount: 3)]
+    [MinColumn, MaxColumn, MedianColumn]
+    public class PolicyManagementBenchmark
+    {
+        private readonly Enforcer _enforcer;
+
+        public PolicyManagementBenchmark()
+        {
+            _enforcer = new Enforcer("examples/rbac_model.conf");
+        }
+
+        [Params(10, 100, 1000)] 
+        public int Times { get; set; }
+
+        [Params(50, 500, 5000)] 
+        public int PolicyCount { get; set; }
+
+        [GlobalSetup(Targets = new []{nameof(AddPolicy), nameof(HasPolicy)})]
+        public void GlobalSetupForHasPolicy()
+        {
+            var random = new Random();
+            for (var i = 0; i < PolicyCount; i++)
+            {
+                _enforcer.AddPolicy($"group{random.Next(0, PolicyCount * 20)}", $"obj{i}", "read");
+            }
+
+            Console.WriteLine($"Already set {PolicyCount} policy.");
+        }
+
+        [Benchmark]
+        public void HasPolicy()
+        {
+            for (var i = 0; i < Times; i++)
+            {
+                _enforcer.HasPolicy($"name{i}", $"data{i}", "read");
+            }
+        }
+
+        [Benchmark]
+        public void AddPolicy()
+        {
+            for (var i = 0; i < Times; i++)
+            {
+                _enforcer.AddPolicy($"name{i}", $"data{i}", "read");
+            }
+        }
+    }
+}
---
+++ NetCasbin.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace NetCasbin.Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var _ = BenchmarkRunner.Run<PolicyManagementBenchmark>();
+        }
+    }
+}
